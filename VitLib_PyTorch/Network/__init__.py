e180413b1b30dd60d5b9909df6bc32200edd4435
Kotetsu0000 <yuuiti1234@gmail.com>
2024-09-15 18:33:12 +0900

    fix

--- VitLib_PyTorch/Network/__init__.py
+++ VitLib_PyTorch/Network/__init__.py
@@ -1,7 +1,7 @@
 from .module import calc_sum_params
 from .U_Net import U_Net, Discriminator
 from .Nested_U_Net import Nested_U_Net
-from .Swin_Transformer_U_Net import Swin_Transformer_U_Net
+from .Swin_Transformer_U_Net import SwinUnet
 from .ResNet import resnet18, resnet34, resnet50, resnet101, resnet152
 from .Deeplabv3 import DeepLabV3_resnet
 from .Attention_U_Net import Attention_U_Net
